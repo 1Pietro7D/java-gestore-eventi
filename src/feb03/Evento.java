5bde0892db51c643ea1e0db461aa740d71522d7b
PietroDanieli <1pie7d@gmail.com>
2023-02-03 12:34:52 +0100

    managment reserve method

--- src/feb03/Evento.java
+++ src/feb03/Evento.java
@@ -3,76 +3,85 @@ package feb03;
 import java.time.LocalDate;
 
 public class Evento {
-    private String titolo;
-    private LocalDate data;
-    private int postiTotali;
-    private int postiPrenotati;
+	private String title;
+	private LocalDate date;
+	private int totalSeats;
+	private int reserveSeats;
 
-    public Evento(String titolo, LocalDate data, int postiTotali) {
-        if (data.isBefore(LocalDate.now()) || postiTotali <= 0) {
-            throw new IllegalArgumentException("Data non valida o numero di posti totali non positivo");
-        }
-        this.titolo = titolo;
-        this.data = data;
-        this.postiTotali = postiTotali;
-        this.postiPrenotati = 0;
-    }
+	public Evento(String title, LocalDate date, int totalSeats) {
+		if (date.isBefore(LocalDate.now()) || totalSeats <= 0) {
+			throw new IllegalArgumentException("Data non valida o numero di posti totali non positivo");
+		}
+		this.title = title;
+		this.date = date;
+		this.totalSeats = totalSeats;
+		this.reserveSeats = 0;
+	}
 
-    public String getTitolo() {
-        return titolo;
-    }
+	public String getTitolo() {
+		return title;
+	}
 
-    public void setTitolo(String titolo) {
-        this.titolo = titolo;
-    }
+	public void setTitolo(String title) {
+		this.title = title;
+	}
 
-    public LocalDate getData() {
-        return data;
-    }
+	public LocalDate getDate() {
+		return date;
+	}
 
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 
-    public int getPostiTotali() {
-        return postiTotali;
-    }
+	public int geTotalSeats() {
+		return totalSeats;
+	}
 
-    public int getPostiPrenotati() {
-        return postiPrenotati;
-    }
+	public int getReserveSeats() {
+		return reserveSeats;
+	}
 
-    public void prenota() {
-        if (data.isBefore(LocalDate.now()) || postiPrenotati >= postiTotali) {
-            throw new IllegalArgumentException("Impossibile prenotare: evento già passato o posti esauriti");
-        }
-        postiPrenotati++;
-    }
+	public void reserve() {
+		if (date.isBefore(LocalDate.now()) || reserveSeats >= totalSeats) {
+			throw new IllegalArgumentException("Impossibile prenotare: evento già passato o posti esauriti");
+		}
+		reserveSeats++;
+	}
+	//overloading reserve()
+	public void reserve(int numSeats) throws Exception {
+		if (date.isBefore(LocalDate.now()) || reserveSeats + numSeats >= totalSeats) {
+			throw new IllegalArgumentException("Impossibile prenotare: evento già passato o posti esauriti");
+		} else if (numSeats <= 0) {
+			throw new Exception("Impossibile prenotare: valore non valido");
+		}
+		reserveSeats += numSeats;
+	}
 
-    public void disdici() {
-        if (data.isBefore(LocalDate.now()) || postiPrenotati <= 0) {
-            throw new IllegalArgumentException("Impossibile disdire: evento già passato o nessuna prenotazione effettuata");
-        }
-        postiPrenotati--;
-    }
+	public void disdici() {
+		if (date.isBefore(LocalDate.now()) || reserveSeats <= 0) {
+			throw new IllegalArgumentException(
+					"Impossibile disdire: evento già passato o nessuna prenotazione effettuata");
+		}
+		reserveSeats--;
+	}
 
-    @Override
-    public String toString() {
-        return data.toString() + " + " + titolo + "+ Posti totali: " + postiTotali + " + Posti prenotati: " + postiPrenotati;
-    }
+	@Override
+	public String toString() {
+		return date.toString() + " + " + title + "+ Posti totali: " + totalSeats + " + Posti prenotati: "
+				+ reserveSeats;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Evento e = new Evento("mario", LocalDate.of(2024, 1, 1), 5);
-		e.prenota();
-		e.prenota();
-		e.prenota();
-		e.prenota();
-		e.prenota();
-		/*try {e.prenota();}catch(IllegalArgumentException e1){
-			System.out.println(e1.getMessage());
-		} */
-		System.out.println(e);
+		Evento ev= new Evento("mario", LocalDate.of(2024, 1, 1), 5);
+		try {
+			ev.reserve(5);
+			System.out.println(ev);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
+
+	}
 
 }
